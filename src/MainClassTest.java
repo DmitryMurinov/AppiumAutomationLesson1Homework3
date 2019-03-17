603d27371303f0b439a3a3678523eb595325a7e3
Dmitry Murinov <murinov.dmitry.acc@gmail.com>
2019-03-17 22:56:46 +0300

    Поправил импорты.

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -1,7 +1,6 @@
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.*;
-import static org.hamcrest.CoreMatchers.anyOf;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class MainClassTest {
