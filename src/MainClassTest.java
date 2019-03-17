d3d03092734a7d76a40707e8ed44db4b8b11bf7e
Dmitry Murinov <murinov.dmitry.acc@gmail.com>
2019-03-17 22:55:29 +0300

    Initial commit

---
+++ src/MainClassTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.CoreMatchers.anyOf;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class MainClassTest {
+
+    @Test
+    public void testGetClassString() {
+        MainClass mainClass = new MainClass(); //not sure should I make it field of the class, so incapsulated it here.
+        assertThat("String doesn't match expected value: ", mainClass.getClassString(),
+                                    anyOf(containsString("hello"), containsString("Hello")));
+    }
+
+}
